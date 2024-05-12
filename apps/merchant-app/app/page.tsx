75d9ff930b348065dc845375173acde4bd2dbfe5
victor <chrollolucilfer1402@gmail.com>
2024-05-12 22:56:52 +0530

    added some stuff

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 
 import { useBalance } from "@repo/store/balance";
+import { Appbar } from "@repo/ui/appbar";
 
 export default function () {
   const balance = useBalance();
-  return <div>hi there {balance}</div>;
+
+  const onSignIn = () => {
+    console.log("signed in");
+  };
+
+  const onSignOut = () => {
+    console.log("signed in");
+  };
+
+  return (
+    <>
+      <Appbar
+        user={{ name: "victor" }}
+        onSignin={onSignIn}
+        onSignout={onSignOut}
+      />
+      <div>hi there </div>
+      <div>Balance: 200 </div>;
+    </>
+  );
 }
