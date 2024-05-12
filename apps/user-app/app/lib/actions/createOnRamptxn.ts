886a4fe9e335be852274bc478f70b2a1f7f0b72d
victor <chrollolucilfer1402@gmail.com>
2024-05-12 22:10:59 +0530

    added building action, and formatted some code files

--- apps/user-app/app/lib/actions/createOnRamptxn.ts
+++ apps/user-app/app/lib/actions/createOnRamptxn.ts
@@ -4,27 +4,30 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
-export async function createOnRampTransaction(amount: number, provider: string) {
-    const session = await getServerSession(authOptions);
-    const token = Math.random().toString();
-    const userId = session.user.id;
-    if (!userId) {
-        return {
-            message: "User not logged in"
-        }
-    }
-    await prisma.onRampTransaction.create({
-        data: {
-            userId: Number(userId), // 1
-            amount: amount,
-            status: "Processing",
-            startTime: new Date(),
-            provider,
-            token: token
-        }
-    })
-
+export async function createOnRampTransaction(
+  amount: number,
+  provider: string,
+) {
+  const session = await getServerSession(authOptions);
+  const token = Math.random().toString();
+  const userId = session.user.id;
+  if (!userId) {
     return {
-        message: "On ramp transaction added"
-    }
+      message: "User not logged in",
+    };
+  }
+  await prisma.onRampTransaction.create({
+    data: {
+      userId: Number(userId), // 1
+      amount: amount,
+      status: "Processing",
+      startTime: new Date(),
+      provider,
+      token: token,
+    },
+  });
+
+  return {
+    message: "On ramp transaction added",
+  };
 }
