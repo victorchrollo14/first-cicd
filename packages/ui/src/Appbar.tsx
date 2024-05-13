2c535c1eaae8cb58066b4386183989a83048099a
victor <chrollolucilfer1402@gmail.com>
2024-05-13 22:37:11 +0530

    update appbar

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -11,8 +11,14 @@ interface AppbarProps {
 
 export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
   return (
-    <div className="flex justify-between border-b px-4 border-slate-300">
+    <div className="flex justify-between items-center border-b px-4 border-slate-300">
       <div className="text-lg flex flex-col justify-center">PayTM</div>
+      <nav className="flex flex-row items-center gap-3">
+        <h2>Home</h2>
+        <h2>Transactions</h2>
+        <h2>Transfer</h2>
+        <h2>P2P Transfer</h2>
+      </nav>
       <div className="flex flex-col justify-center pt-2">
         <Button onClick={user ? onSignout : onSignin}>
           {user ? "Logout" : "Login"}
